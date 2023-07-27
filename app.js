8c362ba517a3315b5bce06c7429ed933d17ef96d
YXShang97 <yxshang97@gmail.com>
2023-07-27 14:10:28 -0400

    ExpressError

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const HikingTrail = require("./models/hikingtrail");
 
@@ -26,43 +28,67 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/hikingtrails", async (req, res) => {
-  const hikingtrails = await HikingTrail.find({});
-  res.render("hikingtrails/index", { hikingtrails });
-});
+app.get(
+  "/hikingtrails",
+  catchAsync(async (req, res) => {
+    const hikingtrails = await HikingTrail.find({});
+    res.render("hikingtrails/index", { hikingtrails });
+  })
+);
 
 app.get("/hikingtrails/new", (req, res) => {
   res.render("hikingtrails/new");
 });
 
-app.post("/hikingtrails", async (req, res) => {
-  const hikingtrail = new HikingTrail(req.body.hikingtrail);
-  await hikingtrail.save();
-  res.redirect(`/hikingtrails/${hikingtrail._id}`);
-});
+app.post(
+  "/hikingtrails",
+  catchAsync(async (req, res) => {
+    const hikingtrail = new HikingTrail(req.body.hikingtrail);
+    await hikingtrail.save();
+    res.redirect(`/hikingtrails/${hikingtrail._id}`);
+  })
+);
 
-app.get("/hikingtrails/:id", async (req, res) => {
-  const hikingtrail = await HikingTrail.findById(req.params.id);
-  res.render("hikingtrails/show", { hikingtrail });
-});
+app.get(
+  "/hikingtrails/:id",
+  catchAsync(async (req, res) => {
+    const hikingtrail = await HikingTrail.findById(req.params.id);
+    res.render("hikingtrails/show", { hikingtrail });
+  })
+);
 
-app.get("/hikingtrails/:id/edit", async (req, res) => {
-  const hikingtrail = await HikingTrail.findById(req.params.id);
-  res.render("hikingtrails/edit", { hikingtrail });
-});
+app.get(
+  "/hikingtrails/:id/edit",
+  catchAsync(async (req, res) => {
+    const hikingtrail = await HikingTrail.findById(req.params.id);
+    res.render("hikingtrails/edit", { hikingtrail });
+  })
+);
 
-app.put("/hikingtrails/:id", async (req, res) => {
-  const { id } = req.params;
-  const hikingtrail = await HikingTrail.findByIdAndUpdate(id, {
-    ...req.body.hikingtrail,
-  });
-  res.redirect(`/hikingtrails/${hikingtrail._id}`);
-});
+app.put(
+  "/hikingtrails/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const hikingtrail = await HikingTrail.findByIdAndUpdate(id, {
+      ...req.body.hikingtrail,
+    });
+    res.redirect(`/hikingtrails/${hikingtrail._id}`);
+  })
+);
 
-app.delete("/hikingtrails/:id", async (req, res) => {
-  const { id } = req.params;
-  await HikingTrail.findByIdAndDelete(id);
-  res.redirect("/hikingtrails");
+app.delete(
+  "/hikingtrails/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await HikingTrail.findByIdAndDelete(id);
+    res.redirect("/hikingtrails");
+  })
+);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
